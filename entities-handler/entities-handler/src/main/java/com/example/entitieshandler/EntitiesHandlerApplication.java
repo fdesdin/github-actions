d33384456aba37ab27cf29cec9632ef94e2cbdac
David <fdesdin@gmail.com>
2023-11-28 11:12:28 -0700

    Adding new projects
    Added "entities" and "entities-handler" projects to main branch

---
+++ entities-handler/entities-handler/src/main/java/com/example/entitieshandler/EntitiesHandlerApplication.java
@@ -0,0 +1,17 @@
+package com.example.entitieshandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.entities.Student;
+
+@SpringBootApplication
+public class EntitiesHandlerApplication {	
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntitiesHandlerApplication.class, args);
+		Student a = new Student("David Felipe", 33, 'M');
+		System.out.println(a);
+	}
+
+}
---
+++ entities-handler/entities-handler/src/test/java/com/example/entitieshandler/EntitiesHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entitieshandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntitiesHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
