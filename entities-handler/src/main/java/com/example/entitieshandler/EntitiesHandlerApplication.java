d23034d1cc2e3fe5b786142ea247232209854aef
David <fdesdin@gmail.com>
2023-12-01 11:38:25 -0700

    Update EntitiesHandlerApplication.java

--- entities-handler/src/main/java/com/example/entitieshandler/EntitiesHandlerApplication.java
+++ entities-handler/src/main/java/com/example/entitieshandler/EntitiesHandlerApplication.java
@@ -11,7 +11,7 @@ public class EntitiesHandlerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EntitiesHandlerApplication.class, args);
 		Student a = new Student("David Felipe", 33, 'M');
-//		System.out.println("New Student");
+		System.out.println("New Student");
 		System.out.println(a);
 	}
 
