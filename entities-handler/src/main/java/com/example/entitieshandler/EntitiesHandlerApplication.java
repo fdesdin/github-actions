14e627b9771222a95684386e92e48043cdb26214
David <fdesdin@gmail.com>
2023-12-01 11:52:29 -0700

    Update EntitiesHandlerApplication.java
    excluding entities from entities-handler

--- entities-handler/src/main/java/com/example/entitieshandler/EntitiesHandlerApplication.java
+++ entities-handler/src/main/java/com/example/entitieshandler/EntitiesHandlerApplication.java
@@ -3,16 +3,14 @@ package com.example.entitieshandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.entities.Student;
-
 @SpringBootApplication
 public class EntitiesHandlerApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntitiesHandlerApplication.class, args);
-		Student a = new Student("David Felipe", 33, 'M');
-		System.out.println("New Student");
-		System.out.println(a);
+		// Student a = new Student("David Felipe", 33, 'M');
+		// System.out.println("New Student");
+		// System.out.println(a);
 	}
 
 }
